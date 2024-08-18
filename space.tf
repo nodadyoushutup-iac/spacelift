a9e1061cc8bb9bd03dd3fa9fe000fce2a34bf2e3
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-18 23:30:25 +0000

    [WIP] Spacelift

--- space.tf
+++ space.tf
@@ -5,3 +5,16 @@ resource "spacelift_space" "infra" {
   parent_space_id = "root"
   inherit_entities = true
 }
+
+resource "spacelift_space" "init" {
+  name = "init"
+  parent_space_id = "root"
+  inherit_entities = true
+}
+
+resource "spacelift_space" "config" {
+  name = "config"
+  parent_space_id = "root"
+  inherit_entities = true
+}
+
