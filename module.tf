b8f15711772b97fe1bee90bb09bf23286fe92b64
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-18 23:55:14 +0000

    [WIP] Spacelift

--- module.tf
+++ module.tf
@@ -1,43 +1,43 @@
-resource "spacelift_module" "proxmox_virtual_machine_simple" {
-  name               = "virtual_machine_simple"
-  terraform_provider = "proxmox"
-  administrative     = false
-  branch             = "main"
-  description        = "Virtual Machine (Simple)"
-  repository         = "terraform"
-  project_root       = "modules/proxmox/virtual_machine_simple"
-  labels             = ["infra", "proxmox"]
-}
+# resource "spacelift_module" "proxmox_virtual_machine" {
+#   name               = "virtual_machine"
+#   terraform_provider = "proxmox"
+#   administrative     = false
+#   branch             = "main"
+#   description        = "Virtual Machine (Simple)"
+#   repository         = "terraform"
+#   project_root       = "modules/proxmox/virtual_machine"
+#   labels             = ["infra", "proxmox"]
+# }
 
-resource "spacelift_module" "fortigate_vip" {
-  name               = "vip"
-  terraform_provider = "fortigate"
-  administrative     = false
-  branch             = "main"
-  description        = "Fortigate Firewall Virtual IP"
-  repository         = "terraform"
-  project_root       = "modules/fortigate/vip"
-  labels             = ["config", "fortigate"]
-}
+# resource "spacelift_module" "fortigate_vip" {
+#   name               = "vip"
+#   terraform_provider = "fortigate"
+#   administrative     = false
+#   branch             = "main"
+#   description        = "Fortigate Firewall Virtual IP"
+#   repository         = "terraform"
+#   project_root       = "modules/fortigate/vip"
+#   labels             = ["config", "fortigate"]
+# }
 
-resource "spacelift_module" "fortigate_policy" {
-  name               = "policy"
-  terraform_provider = "fortigate"
-  administrative     = false
-  branch             = "main"
-  description        = "Fortigate Firewall Policy"
-  repository         = "terraform"
-  project_root       = "modules/fortigate/policy"
-  labels             = ["config", "fortigate"]
-}
+# resource "spacelift_module" "fortigate_policy" {
+#   name               = "policy"
+#   terraform_provider = "fortigate"
+#   administrative     = false
+#   branch             = "main"
+#   description        = "Fortigate Firewall Policy"
+#   repository         = "terraform"
+#   project_root       = "modules/fortigate/policy"
+#   labels             = ["config", "fortigate"]
+# }
 
-resource "spacelift_module" "fortigate_port_forward" {
-  name               = "port_forward"
-  terraform_provider = "fortigate"
-  administrative     = false
-  branch             = "main"
-  description        = "Fortigate Port Forward"
-  repository         = "terraform"
-  project_root       = "modules/fortigate/port_forward"
-  labels             = ["config", "fortigate"]
-}
+# resource "spacelift_module" "fortigate_port_forward" {
+#   name               = "port_forward"
+#   terraform_provider = "fortigate"
+#   administrative     = false
+#   branch             = "main"
+#   description        = "Fortigate Port Forward"
+#   repository         = "terraform"
+#   project_root       = "modules/fortigate/port_forward"
+#   labels             = ["config", "fortigate"]
+# }
