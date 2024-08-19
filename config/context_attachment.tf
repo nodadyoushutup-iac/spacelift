e3a4af343daef6eabbf9b0b8b6668abbf7e28499
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 02:54:46 +0000

    [WIP] Spacelift

--- config/context_attachment.tf
+++ config/context_attachment.tf
@@ -3,6 +3,6 @@
 resource "spacelift_context_attachment" "context_attachment_debug" {
   for_each = data.spacelift_stacks.stacks
   context_id = "debug"
-  stack_id   = each.value.stack_id
+  stack_id   = each.value.id
   priority   = 0
 }
