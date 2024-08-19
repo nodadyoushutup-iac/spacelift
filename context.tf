1623dd7b3f70624824c5e78eef84a370d4aedfdb
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 00:31:51 +0000

    [WIP] Spacelift

--- context.tf
+++ context.tf
@@ -6,6 +6,12 @@ resource "spacelift_context" "provider" {
   space_id = "root"
 }
 
+resource "spacelift_context" "spacelift" {
+  description = "Spacelift configuration"
+  name = "spacelift"
+  space_id = spacelift_space.infra.id
+}
+
 resource "spacelift_context" "virtual_machine" {
   description = "Virtual Machine congfiguration"
   name = "virtual_machine"
