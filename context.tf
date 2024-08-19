6a5865cfc985a5064a01c0a80154f5415823a454
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 01:13:52 +0000

    [WIP] Spacelift

--- context.tf
+++ context.tf
@@ -1,33 +1,33 @@
-# context.tf
+# # context.tf
 
-data "spacelift_space" "provider" {
-  space_id = "provider"
-}
+# data "spacelift_space" "provider" {
+#   space_id = "provider"
+# }
 
-data "spacelift_space" "infra" {
-  space_id = "infra"
-}
+# data "spacelift_space" "infra" {
+#   space_id = "infra"
+# }
 
-data "spacelift_space" "config" {
-  space_id = "config"
-}
+# data "spacelift_space" "config" {
+#   space_id = "config"
+# }
 
 
 
-resource "spacelift_context" "provider" {
-  description = "Provider congfiguration"
-  name = "provider"
-  space_id = "root"
-}
+# resource "spacelift_context" "provider" {
+#   description = "Provider congfiguration"
+#   name = "provider"
+#   space_id = "root"
+# }
 
-resource "spacelift_context" "spacelift" {
-  description = "Spacelift configuration"
-  name = "spacelift"
-  space_id = data.spacelift_space.infra.id
-}
+# resource "spacelift_context" "spacelift" {
+#   description = "Spacelift configuration"
+#   name = "spacelift"
+#   space_id = data.spacelift_space.infra.id
+# }
 
-resource "spacelift_context" "virtual_machine" {
-  description = "Virtual Machine congfiguration"
-  name = "virtual_machine"
-  space_id = data.spacelift_space.infra.id
-}
+# resource "spacelift_context" "virtual_machine" {
+#   description = "Virtual Machine congfiguration"
+#   name = "virtual_machine"
+#   space_id = data.spacelift_space.infra.id
+# }
--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
-variable "github_branch" {
-  type = string
-  description = "Github branch name"
-  default = "main"
-}
+# variable "github_branch" {
+#   type = string
+#   description = "Github branch name"
+#   default = "main"
+# }
