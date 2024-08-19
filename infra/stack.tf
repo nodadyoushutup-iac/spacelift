445217ae58f934ab1879575b1b567e4c222f5d49
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 05:05:49 +0000

    [WIP] Spacelift

--- infra/stack.tf
+++ infra/stack.tf
@@ -1,19 +1,23 @@
 # stack.tf
 
-resource "spacelift_stack" "stack" {
-  for_each = local.spacelift.stack
-
-  # space_id = each.value.space_id
-  administrative  = each.value.administrative
-  autodeploy = each.value.autodeploy
-  branch = each.value.branch
-  description = each.value.description
-  name = each.value.name
-  repository = each.value.repository
-  project_root = each.value.project_root
-  terraform_version = each.value.terraform_version
-  labels = concat(each.value.labels, ["all"])
-  github_enterprise { 
-    namespace = each.value.enterprise
-  }
+output "yaml" {
+  value = local.spacelift.stack
 }
+
+# resource "spacelift_stack" "stack" {
+#   for_each = local.spacelift.stack
+
+#   # space_id = each.value.space_id
+#   administrative  = each.value.administrative
+#   autodeploy = each.value.autodeploy
+#   branch = each.value.branch
+#   description = each.value.description
+#   name = each.value.name
+#   repository = each.value.repository
+#   project_root = each.value.project_root
+#   terraform_version = each.value.terraform_version
+#   labels = concat(each.value.labels, ["all"])
+#   github_enterprise { 
+#     namespace = each.value.enterprise
+#   }
+# }
