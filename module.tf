d138f0b4aea8646a04aedff263957b4f1e8bc0ed
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 00:03:53 +0000

    [WIP] Spacelift

--- module.tf
+++ module.tf
@@ -12,35 +12,44 @@ resource "spacelift_module" "proxmox_virtual_machine" {
     }
 }
 
-# resource "spacelift_module" "fortigate_vip" {
-#   name               = "vip"
-#   terraform_provider = "fortigate"
-#   administrative     = false
-#   branch             = "main"
-#   description        = "Fortigate Firewall Virtual IP"
-#   repository         = "terraform"
-#   project_root       = "modules/fortigate/vip"
-#   labels             = ["config", "fortigate"]
-# }
+resource "spacelift_module" "fortigate_vip" {
+    name               = "vip"
+    terraform_provider = "fortigate"
+    administrative     = false
+    branch             = "main"
+    description        = "Fortigate Firewall Virtual IP"
+    repository         = "module"
+    project_root       = "fortigate/vip"
+    labels             = ["config", "fortigate"]
+    github_enterprise { 
+        namespace = "nodadyoushutup-terraform"
+    }   
+}
 
-# resource "spacelift_module" "fortigate_policy" {
-#   name               = "policy"
-#   terraform_provider = "fortigate"
-#   administrative     = false
-#   branch             = "main"
-#   description        = "Fortigate Firewall Policy"
-#   repository         = "terraform"
-#   project_root       = "modules/fortigate/policy"
-#   labels             = ["config", "fortigate"]
-# }
+resource "spacelift_module" "fortigate_policy" {
+    name               = "policy"
+    terraform_provider = "fortigate"
+    administrative     = false
+    branch             = "main"
+    description        = "Fortigate Firewall Policy"
+    repository         = "module"
+    project_root       = "fortigate/policy"
+    labels             = ["config", "fortigate"]
+    github_enterprise { 
+        namespace = "nodadyoushutup-terraform"
+    }
+}
 
-# resource "spacelift_module" "fortigate_port_forward" {
-#   name               = "port_forward"
-#   terraform_provider = "fortigate"
-#   administrative     = false
-#   branch             = "main"
-#   description        = "Fortigate Port Forward"
-#   repository         = "terraform"
-#   project_root       = "modules/fortigate/port_forward"
-#   labels             = ["config", "fortigate"]
-# }
+resource "spacelift_module" "fortigate_port_forward" {
+    name               = "port_forward"
+    terraform_provider = "fortigate"
+    administrative     = false
+    branch             = "main"
+    description        = "Fortigate Port Forward"
+    repository         = "module"
+    project_root       = "fortigate/port_forward"
+    labels             = ["config", "fortigate"]
+    github_enterprise { 
+        namespace = "nodadyoushutup-terraform"
+    }
+}
