79fb7944bee3910c39538bdeddf190c5cc52600e
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 01:42:39 +0000

    [WIP] Spacelift

--- context_attachment.tf
+++ context_attachment.tf
@@ -8,8 +8,13 @@ data "spacelift_context" "spacelift" {
   context_id = "spacelift"
 }
 
-resource "spacelift_context_attachment" "spacelift_spacelift" {
-  context_id = data.spacelift_context.spacelift.name
-  stack_id   = data.spacelift_stack.spacelift.name
-  priority   = 0
+# resource "spacelift_context_attachment" "spacelift_spacelift" {
+#   context_id = data.spacelift_context.spacelift.name
+#   stack_id   = data.spacelift_stack.spacelift.name
+#   priority   = 0
+# }
+
+
+output "spacelift_context" {
+  value = data.spacelift_context.spacelift
 }
