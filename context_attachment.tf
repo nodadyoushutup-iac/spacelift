e10125da67d4287b82c6422522f202f216290379
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 01:53:50 +0000

    [WIP] Spacelift

--- context_attachment.tf
+++ context_attachment.tf
@@ -1,24 +1,23 @@
-# # context_attachment.tf
-
-data "spacelift_stack" "proxmox_infra" {
-  stack_id = "proxmox_infra"
-}
+# context_attachment.tf
 
 data "spacelift_context" "provider" {
   context_id = "provider"
 }
 
-# resource "spacelift_context_attachment" "spacelift_spacelift" {
-#   context_id = data.spacelift_context.spacelift.name
-#   stack_id   = data.spacelift_stack.spacelift.name
-#   priority   = 0
-# }
+data "spacelift_stack" "proxmox_infra" {
+  stack_id = "proxmox_infra"
+}
 
-
-output "spacelift_stack" {
-  value = data.spacelift_context.spacelift
+resource "spacelift_context_attachment" "spacelift_spacelift" {
+  context_id = data.spacelift_context.provider.id
+  stack_id   = data.spacelift_stack.proxmox_infra.id
+  priority   = 0
 }
 
 output "spacelift_context" {
   value = data.spacelift_stack.proxmox_infra
+}
+
+output "spacelift_stack" {
+  value = data.spacelift_context.spacelift
 }
