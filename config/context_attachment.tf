db50404681d42a309b492bfca4c6a2ae1b900848
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 03:33:48 +0000

    [WIP] Spacelift

--- config/context_attachment.tf
+++ config/context_attachment.tf
@@ -10,12 +10,12 @@ output "stacks" {
   value = data.spacelift_stacks.stacks.stacks
 }
 
-resource "spacelift_context_attachment" "context_attachment_debug" {
-  for_each = {
-    for stack in data.spacelift_stacks.stacks.stacks : stack.stack_id => stack
-  }
+# resource "spacelift_context_attachment" "context_attachment_debug" {
+#   for_each = {
+#     for stack in data.spacelift_stacks.stacks.stacks : stack.stack_id => stack
+#   }
 
-  context_id = "debug"
-  stack_id   = each.value.stack_id
-  priority   = 0
-}
+#   context_id = "debug"
+#   stack_id   = each.value.stack_id
+#   priority   = 0
+# }
