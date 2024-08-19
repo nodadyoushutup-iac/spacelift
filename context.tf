7844eeb9f54700e1010b62bf7b421290fb9d7510
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-19 01:34:43 +0000

    [WIP] Spacelift

--- context.tf
+++ context.tf
@@ -1,15 +1,15 @@
 # # context.tf
 
 data "spacelift_space" "init" {
-  space_id = "init"
+  space_id = spacelift_space.init.id
 }
 
 data "spacelift_space" "infra" {
-  space_id = "infra"
+  space_id = spacelift_space.infra.id
 }
 
 data "spacelift_space" "config" {
-  space_id = "config"
+  space_id = spacelift_space.config.id
 }
 
 
