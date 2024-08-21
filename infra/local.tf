20cefc9bd776ccc6c400dffe5b9fbd9bdeb4372e
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-21 23:06:44 +0000

    [WIP] Spacelift

--- infra/local.tf
+++ infra/local.tf
@@ -2,5 +2,5 @@
 
 locals {
   spacelift = yamldecode(file("/mnt/workspace/spacelift.yaml"))
-  config = yamldecode(file("/mnt/workspace/config.yaml"))
+  config = try(yamldecode(file("/mnt/workspace/config.yaml")), yamldecode(file("/mnt/workspace/config.yml")))
 }
