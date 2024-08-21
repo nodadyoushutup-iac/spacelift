4fc979d7a3b9a8b357e99ab26d6f04364d6eae19
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-21 23:42:29 +0000

    [WIP] Spacelift

--- infra/stack.tf
+++ infra/stack.tf
@@ -45,7 +45,7 @@ resource "spacelift_stack" "database" {
     name               = try(local.config.stack.database.name, "database_infra")
     repository         = try(local.config.stack.database.repository, "database")
     project_root       = try(local.config.stack.database.project_root, "infra")
-    terraform_version  = try(local.config.stack.database.terraform_version, "1.5.7")
+    terraform_version  = try(local.config.stack.database.terraform_version, try(local.config.global.terraform.version, "1.5.7"))
     labels          = try(local.config.stack.database.labels, ["all", "infra", "database"])
     github_enterprise { 
         namespace = try(local.config.stack.database.github_enterprise.namespace, "nodadyoushutup-terraform")
