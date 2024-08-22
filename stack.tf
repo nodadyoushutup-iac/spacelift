3e60a5d15cc78bcd93955e9c5351dbd1a21c6fe5
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 02:14:54 +0000

    [WIP] Spacelift

--- stack.tf
+++ stack.tf
@@ -106,3 +106,88 @@ module "database_infra" {
     )
   }
 }
+
+module "vault_infra" {
+  source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+  count = try(contains(local.config.component, "vault")) ? 1 : 0
+
+  # REQUIRED
+  name = try(local.config.stack.vault.name, "vault_infra")
+  repository = try(local.config.stack.vault.repository, "vault")
+
+  # UNIQUE
+  description = try(
+    try(
+        local.config.stack.vault.description, 
+        local.config.global.stack.description
+    ), 
+    "Vault infrastructure"
+  )
+  project_root = try(
+    try(
+      local.config.stack.vault.project_root, 
+      local.config.global.stack.project_root
+    ), 
+    "infra"
+  )
+  labels = try(
+    try(
+      concat(local.config.stack.vault.labels, ["infra", "vault"]), 
+      concat(local.config.global.stack.labels, ["infra", "vault"])
+    ),
+    ["infra", "vault"]
+  )
+
+  # OPTIONAL
+  space_id = try(
+    try(
+      local.config.stack.vault.space_id, 
+      local.config.global.stack.space_id
+    ), 
+    null
+  )
+  administrative = try(
+    try(
+      local.config.stack.vault.administrative, 
+      local.config.global.stack.administrative
+    ), 
+    null
+  )
+  autodeploy = try(
+    try(
+      local.config.stack.vault.autodeploy, 
+      local.config.global.stack.autodeploy
+    ), 
+    null
+  )
+  branch = try(
+    try(
+      local.config.stack.vault.branch, 
+      local.config.global.stack.branch
+    ), 
+    null
+  )
+  terraform_version = try(
+    try(
+      local.config.stack.vault.terraform_version, 
+      local.config.global.stack.terraform_version
+    ), 
+    null
+  )
+  context_priority = try(
+    try(
+      local.config.stack.vault.context_priority, 
+      local.config.global.stack.context_priority
+    ), 
+    null
+  )
+  github_enterprise = { 
+    namespace = try(
+      try(
+        local.config.stack.vault.github_enterprise.namespace, 
+        local.config.global.stack.github_enterprise.namespace
+      ),
+      null
+    )
+  }
+}
