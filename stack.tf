2139dd5da01b28b60ed1a589f3405451708646c4
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 02:23:47 +0000

    [WIP] Spacelift

--- stack.tf
+++ stack.tf
@@ -22,90 +22,90 @@
 #   priority   = 100
 # }
 
-module "database_infra" {
-  source  = "spacelift.io/nodadyoushutup/stack/spacelift"
-  count = try(contains(local.config.component, "database")) ? 1 : 0
+# module "database_infra" {
+#   source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+#   count = try(contains(local.config.component, "database")) ? 1 : 0
 
-  # REQUIRED
-  name = try(local.config.stack.database.name, "database_infra")
-  repository = try(local.config.stack.database.repository, "database")
+#   # REQUIRED
+#   name = try(local.config.stack.database.name, "database_infra")
+#   repository = try(local.config.stack.database.repository, "database")
 
-  # UNIQUE
-  description = try(
-    try(
-        local.config.stack.database.description, 
-        local.config.global.stack.description
-    ), 
-    "Database infrastructure"
-  )
-  project_root = try(
-    try(
-      local.config.stack.database.project_root, 
-      local.config.global.stack.project_root
-    ), 
-    "infra"
-  )
-  labels = try(
-    try(
-      concat(local.config.stack.database.labels, ["infra", "database"]), 
-      concat(local.config.global.stack.labels, ["infra", "database"])
-    ),
-    ["infra", "database"]
-  )
+#   # UNIQUE
+#   description = try(
+#     try(
+#         local.config.stack.database.description, 
+#         local.config.global.stack.description
+#     ), 
+#     "Database infrastructure"
+#   )
+#   project_root = try(
+#     try(
+#       local.config.stack.database.project_root, 
+#       local.config.global.stack.project_root
+#     ), 
+#     "infra"
+#   )
+#   labels = try(
+#     try(
+#       concat(local.config.stack.database.labels, ["infra", "database"]), 
+#       concat(local.config.global.stack.labels, ["infra", "database"])
+#     ),
+#     ["infra", "database"]
+#   )
 
-  # OPTIONAL
-  space_id = try(
-    try(
-      local.config.stack.database.space_id, 
-      local.config.global.stack.space_id
-    ), 
-    null
-  )
-  administrative = try(
-    try(
-      local.config.stack.database.administrative, 
-      local.config.global.stack.administrative
-    ), 
-    null
-  )
-  autodeploy = try(
-    try(
-      local.config.stack.database.autodeploy, 
-      local.config.global.stack.autodeploy
-    ), 
-    null
-  )
-  branch = try(
-    try(
-      local.config.stack.database.branch, 
-      local.config.global.stack.branch
-    ), 
-    null
-  )
-  terraform_version = try(
-    try(
-      local.config.stack.database.terraform_version, 
-      local.config.global.stack.terraform_version
-    ), 
-    null
-  )
-  context_priority = try(
-    try(
-      local.config.stack.database.context_priority, 
-      local.config.global.stack.context_priority
-    ), 
-    null
-  )
-  github_enterprise = { 
-    namespace = try(
-      try(
-        local.config.stack.database.github_enterprise.namespace, 
-        local.config.global.stack.github_enterprise.namespace
-      ),
-      null
-    )
-  }
-}
+#   # OPTIONAL
+#   space_id = try(
+#     try(
+#       local.config.stack.database.space_id, 
+#       local.config.global.stack.space_id
+#     ), 
+#     null
+#   )
+#   administrative = try(
+#     try(
+#       local.config.stack.database.administrative, 
+#       local.config.global.stack.administrative
+#     ), 
+#     null
+#   )
+#   autodeploy = try(
+#     try(
+#       local.config.stack.database.autodeploy, 
+#       local.config.global.stack.autodeploy
+#     ), 
+#     null
+#   )
+#   branch = try(
+#     try(
+#       local.config.stack.database.branch, 
+#       local.config.global.stack.branch
+#     ), 
+#     null
+#   )
+#   terraform_version = try(
+#     try(
+#       local.config.stack.database.terraform_version, 
+#       local.config.global.stack.terraform_version
+#     ), 
+#     null
+#   )
+#   context_priority = try(
+#     try(
+#       local.config.stack.database.context_priority, 
+#       local.config.global.stack.context_priority
+#     ), 
+#     null
+#   )
+#   github_enterprise = { 
+#     namespace = try(
+#       try(
+#         local.config.stack.database.github_enterprise.namespace, 
+#         local.config.global.stack.github_enterprise.namespace
+#       ),
+#       null
+#     )
+#   }
+# }
 
 module "vault_infra" {
   source  = "spacelift.io/nodadyoushutup/stack/spacelift"
