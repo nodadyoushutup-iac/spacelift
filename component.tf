d442c50102b3c2d2b359b1bfb60b8808bedcbd10
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 05:29:25 +0000

    [WIP] Spacelift

--- component.tf
+++ component.tf
@@ -1,5 +1,5 @@
 module "components" {
   source  = "spacelift.io/nodadyoushutup/component/spacelift"
-  for_each = { for component in local.config.component.stack : component => component }
+  for_each = { for component in local.config.component.stack.name : component => component }
   component = each.value
 }
