01e32098fb908efe3b8a5986de0e778dd4219324
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 18:51:19 +0000

    [WIP] Spacelift

--- component.tf
+++ component.tf
@@ -1,5 +1,6 @@
 module "component" {
   source  = "spacelift.io/nodadyoushutup/component/spacelift"
   for_each = { for component in local.config.component.name : component => component }
+  
   component = each.value
 }
--- stack.tf
+++ stack.tf
@@ -4,16 +4,22 @@ module "stack" {
         for stack_name, stack_config in local.config.stack : stack_name => stack_config 
         if !contains(local.config.component.name, stack_name)
     }
+
+    ## REQUIRED ##
+    name                = each.value.name
+    repository          = each.value.repository
+
+    ## UNIQUE ##
+    project_root        = each.value.project_root
+    description         = each.value.description
+    labels              = each.value.labels
+
+    ## OPTIONAL ##
     space_id            = each.value.space_id
     administrative      = each.value.administrative
     autodeploy          = each.value.autodeploy
     branch              = each.value.branch
-    description         = each.value.description
-    # name                = each.value.name
-    repository          = each.value.repository
-    project_root        = each.value.project_root
     terraform_version   = each.value.terraform_version
-    labels              = each.value.labels
     github_enterprise = {
         namespace = each.value.github_enterprise.namespace
     }
