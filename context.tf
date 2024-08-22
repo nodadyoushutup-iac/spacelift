8f0a0a30e408709cab4786636d7cb566be3252aa
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 04:22:04 +0000

    [WIP] Spacelift

--- context.tf
+++ context.tf
@@ -3,3 +3,10 @@ resource "spacelift_context" "config" {
   name = "config"
   space_id = "root"
 }
+
+resource "spacelift_context_attachment" "config_spacelift" {
+  context_id = "config"
+  stack_id   = "spacelift"
+  priority   = 0
+  depends_on = [spacelift_context.config]
+}
