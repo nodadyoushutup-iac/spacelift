01a72b71d223e3681d4eb3c38fd28f69ffdfb043
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-23 00:35:27 +0000

    [WIP] Spacelift

--- local.tf
+++ local.tf
@@ -2,13 +2,4 @@
 
 locals {
   config = try(yamldecode(file("/mnt/workspace/config.yaml")), yamldecode(file("/mnt/workspace/config.yml")))
-  # component = {
-  #   database = contains(local.config.component, "database")
-  #   vault = contains(local.config.component, "vault")
-  #   npm = contains(local.config.component, "npm")
-  #   development = contains(local.config.component, "development")
-  #   fortigate = contains(local.config.component, "fortigate")
-  #   k3s = contains(local.config.component, "k3s")
-  #   monitoring = contains(local.config.component, "monitoring")
-  # }
 }
